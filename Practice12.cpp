53447a66a550cf278c0e2637a66bdf30e7df99b7
Savneet6398 <savneetchhatwal@gmail.com>
2017-07-29 16:55:22 +0530

    Using Graphics

---
+++ Practice12.cpp
@@ -0,0 +1,13 @@
+#include <graphics>
+using namespace std;
+
+int main( )
+{
+    initwindow(400, 300, "First Sample");
+    circle(100, 50, 40);
+    while (!kbhit( ))
+    {
+        delay(200);
+    }
+    return 0;
+}
